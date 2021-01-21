853bbfa2d9f7bc1b0abe46f11bac7d0ee315853c
Victor Mercado <51304598+23MOO23@users.noreply.github.com>
2021-01-21 17:58:21 +0800

    java school project

---
+++ Computer Parts mo 'to(OOP,System)/src/system/beta/Merge.java
@@ -0,0 +1,28 @@
+
+package system.beta;
+
+class Merge {
+    void System()
+    {
+         Selection v = new Selection();
+do{
+    v.Choice();
+switch(v.choice)
+    {
+        case 'a':;
+        v.processor();
+        break;
+        case 'b':;
+        v.vcard();
+        break;
+        case 'c':;
+        v.mobo();
+        break;
+        case 'd':;
+        v.record();
+        break;
+        default:break;
+    }
+}while(v.choice!='e'); 
+    }
+}
---
+++ Computer Parts mo 'to(OOP,System)/src/system/beta/SystemBeta.java
@@ -0,0 +1,8 @@
+package system.beta;
+public class SystemBeta {
+    public static void main(String[] args) {
+         Merge c = new Merge();
+         c.System();
+
+}
+}
